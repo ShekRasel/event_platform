d6c3be407f3267162f95ba5d6b07e2892367292f
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-04-29 23:39:53 +0600

    second commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,10 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { createUser, deleteUser, updateUser} from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
+import {CreateUserParams,UpdateUserParams} from '@/types'
  
 export async function POST(req: Request) {
  
@@ -54,35 +55,35 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-  if(eventType === 'user.created') {
+  if (eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
+    const user: CreateUserParams = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name!,
       lastName: last_name!,
       photo: image_url,
-    }
+    };
 
     const newUser = await createUser(user);
 
-    if(newUser) {
+    if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id
         }
-      })
+      });
     }
 
-    return NextResponse.json({ message: 'OK', user: newUser })
+    return NextResponse.json({ message: 'OK', user: newUser });
   }
 
   if (eventType === 'user.updated') {
     const {id, image_url, first_name, last_name, username } = evt.data
 
-    const user = {
+    const user: UpdateUserParams = {
       firstName: first_name!,
       lastName: last_name!,
       username: username!,
